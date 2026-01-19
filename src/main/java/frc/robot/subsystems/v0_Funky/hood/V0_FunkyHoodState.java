a492d817b9436a9e34415e981f79a62d6287fa01
iamabhiveni <asvenigalla@wpi.edu>
2026-01-19 17:22:23 -0500

    Intial Funky hood states

---
+++ src/main/java/frc/robot/subsystems/v0_Funky/hood/V0_FunkyHoodState.java
@@ -0,0 +1,15 @@
+package frc.robot.subsystems.v0_Funky.hood;
+
+public class V0_FunkyHoodState {
+
+    public enum HoodState {
+        RESET_POSE,
+        GET_HEADING,
+        GET_GLOBAL_POSE,
+        ADD_FIELD_LOCALIZER_VISION_MEASUREMENT,
+        SET_GOAL,
+        SET_VOLTAGE,
+        WAIT
+    }
+    
+}
