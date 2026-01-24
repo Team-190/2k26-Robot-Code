0c210b95afe1bd7a5360ff3d9ac07bf8ae27f2aa
ElliotScher <ecscher84@gmail.com>
2026-01-24 16:42:47 -0500

    add linting

--- src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankIO.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankIO.java
@@ -2,13 +2,12 @@ package frc.robot.subsystems.v1_Gamma.swank;
 
 import static edu.wpi.first.units.Units.*;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Current;
 import edu.wpi.first.units.measure.Temperature;
 import edu.wpi.first.units.measure.Voltage;
 import org.littletonrobotics.junction.AutoLog;
-
-import edu.wpi.first.math.geometry.Rotation2d;
 
 public interface V1_GammaSwankIO {
 
@@ -25,22 +24,17 @@ public interface V1_GammaSwankIO {
   /**
    * Updates inputs.
    *
-   * This method should be called at the beginning of the periodic
-   * iteration. It is responsible for updating the values of the
-   * inputs object with the current state of the subsystem.
+   * <p>This method should be called at the beginning of the periodic iteration. It is responsible
+   * for updating the values of the inputs object with the current state of the subsystem.
    *
    * @param inputs The inputs object to be updated.
    */
-  public default void updateInputs(V1_GammaSwankIOInputs inputs) {
-
-  }
+  public default void updateInputs(V1_GammaSwankIOInputs inputs) {}
 
   /**
    * Sets the voltage being passed into the subsystem.
    *
    * @param volts The voltage to be passed into the subsystem.
    */
-  public default void setVoltage(double volts) {
-
-  }
+  public default void setVoltage(double volts) {}
 }
