f49c9dcad61676aac7d55f9e31a1a12acb911676
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2026-01-24 14:16:13 -0500

    Gear Ratio

--- src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankConstants.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankConstants.java
@@ -5,10 +5,10 @@ import com.ctre.phoenix6.signals.InvertedValue;
 public class V1_GammaSwankConstants {
 
   public static final double DRIVE_INERTIA = 0.0;
-  public static final double GEAR_RATIO = 0.0; // TODO: make correct
+  public static final double GEAR_RATIO = 52/12 * 24/18 * 15/14; // TODO: make correct
   public static final int MOTOR_CAN_ID = -1; // TODO: make correct
-  public static final double SUPPLY_CURRENT_LIMIT = 40; // TODO: make correct
-  public static final double STATOR_CURRENT_LIMIT = 40; // TODO: make correct
+  public static final double SUPPLY_CURRENT_LIMIT = 40; 
+  public static final double STATOR_CURRENT_LIMIT = 40; 
   public static final boolean IS_CAN_FD = false;
 
   public static final InvertedValue INVERSION = InvertedValue.CounterClockwise_Positive;
