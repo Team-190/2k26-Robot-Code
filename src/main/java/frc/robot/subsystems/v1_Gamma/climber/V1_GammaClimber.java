41744a49861a43b009d03d3a5f1d15a04a460108
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2026-01-24 13:15:03 -0500

    Redid using GompeiLib Arm
    Co-authored-by: Abhiraam Venigalla <Abhi52897340598327@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/v1_Gamma/climber/V1_GammaClimber.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/climber/V1_GammaClimber.java
@@ -1,33 +1,44 @@
 package frc.robot.subsystems.v1_Gamma.climber;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import org.littletonrobotics.junction.AutoLogOutput;
-import org.littletonrobotics.junction.Logger;
 
-public class V1_GammaClimber {
-  private final V1_GammaClimberIO io;
-  private final V1_GammaClimberIOInputsAutoLogged inputs;
+import org.littletonrobotics.junction.AutoLogOutput;
+import edu.wpi.team190.gompeilib.subsystems.arm.Arm;
+import edu.wpi.team190.gompeilib.subsystems.arm.ArmIO;
+
+public class V1_GammaClimber extends SubsystemBase{
+  private Arm arm;
+  
+  public V1_GammaClimber(ArmIO io) {
+    arm = new Arm(io, this, 1);
+  }
+
+  @Override
+  public void periodic() {
+    arm.periodic();
+  }
+
+  public Command setVoltage(double voltage) {
+    return Commands.runOnce(() -> arm.setVoltage(voltage));
+  }
+
+  public Command setPosition(Rotation2d position) {
+    return Commands.runOnce(() -> arm.setPosition(position));
+  }
+  
+  public Command setPositionGoal(Rotation2d positionGoal) {
+    return Commands.runOnce(() -> arm.setPositionGoal(positionGoal));
+  }
+  
+  public Command stop() {
+    return Commands.runOnce(() -> arm.setVoltage(0));
+  }
 
   @AutoLogOutput(key = "Climber/isClimbed")
   private boolean isClimbed;
-
-  public V1_GammaClimber(V1_GammaClimberIO io) {
-    this.io = io;
-    this.inputs = new V1_GammaClimberIOInputsAutoLogged();
-  }
-
-  public void periodic() {
-    io.updateInputs(inputs);
-    Logger.processInputs("Climber", inputs);
-  }
-
-  public Command runVoltageCommand(double voltage) {
-    return Commands.runOnce(() -> io.setVoltage(voltage));
-  }
-
-  public Command stopCommand() {
-    return Commands.runOnce(() -> io.setVoltage(0));
-  }
+  
 }
+
--- src/main/java/frc/robot/subsystems/v1_Gamma/climber/V1_GammaClimberConstants.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/climber/V1_GammaClimberConstants.java
@@ -1,18 +1,54 @@
 package frc.robot.subsystems.v1_Gamma.climber;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.team190.gompeilib.core.utility.LoggedTunableNumber;
+import edu.wpi.team190.gompeilib.subsystems.arm.ArmConstants;
+import edu.wpi.team190.gompeilib.subsystems.arm.ArmConstants.ArmParameters;
+import edu.wpi.team190.gompeilib.subsystems.arm.ArmConstants.Gains;
+import edu.wpi.team190.gompeilib.subsystems.arm.ArmConstants.Constraints;
+import edu.wpi.team190.gompeilib.subsystems.arm.ArmConstants.CurrentLimits;
+
 
 public class V1_GammaClimberConstants {
-  public static final int MOTOR_CAN_ID = -1; // TODO: Put real data
+  public static final ArmConstants CLIMBER_CONSTANTS = new ArmConstants(
+      42,
+      new ArmParameters(
+          new DCMotor(0, 0, 0, 0, 0, 0),
+          new Rotation2d(),
+          new Rotation2d(),
+          1,
+          12,
+          34.0,
+          45.0,
+          56.0),
+      new Gains(
+          new LoggedTunableNumber("Ks", 0),
+          new LoggedTunableNumber("Kv", 0),
+          new LoggedTunableNumber("Ka", 0),
+          new LoggedTunableNumber("Kg", 0),
+          new LoggedTunableNumber("Kp", 0),
+          new LoggedTunableNumber("Kd", 0)),
+      new Gains(
+          new LoggedTunableNumber("Ks", 0),
+          new LoggedTunableNumber("Kv", 0),
+          new LoggedTunableNumber("Ka", 0),
+          new LoggedTunableNumber("Kg", 0),
+          new LoggedTunableNumber("Kp", 0),
+          new LoggedTunableNumber("Kd", 0)),
+      new Gains(
+          new LoggedTunableNumber("Ks", 0),
+          new LoggedTunableNumber("Kv", 0),
+          new LoggedTunableNumber("Ka", 0),
+          new LoggedTunableNumber("Kg", 0),
+          new LoggedTunableNumber("Kp", 0),
+          new LoggedTunableNumber("Kd", 0)),
+      new Constraints(
+          new LoggedTunableNumber("MaxAccelerationRotationsPerSecondSquared", 0),
+          new LoggedTunableNumber("CruisingVelocityRotationsPerSecondSquared", 0),
+          new LoggedTunableNumber("GoalToleranceRadians", 0)),
+      new CurrentLimits(0, 0, 0),
+      1.0,
+      false);
 
-  public static final double SUPPLY_CURRENT_LIMIT = 40.0;
-  public static final double STATOR_CURRENT_LIMIT = 40.0; 
-
-  public static final double CLIMBER_CLIMBED_L1_RADIANS = 67; // TODO: Put real data
-  public static final double CLIMBER_CLIMBED_L2_RADIANS = -67; // TODO: Put real data
-
-  public static final double GEAR_RATIO = -1; // TODO: Put real data
-  public static final DCMotor MOTOR_CONFIG = DCMotor.getKrakenX60Foc(1); // TODO: Put real data
-
-  public static final boolean IS_CAN_FD = false;
 }
