f971a2032d12fba2becac01a8d08c33dacc75685
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2026-01-24 16:18:43 -0500

    changed flywheel to roller

--- src/main/java/frc/robot/subsystems/v1_Gamma/intake/V1_GammaIntake.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/intake/V1_GammaIntake.java
@@ -3,16 +3,16 @@ package frc.robot.subsystems.v1_Gamma.intake;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.team190.gompeilib.subsystems.generic.flywheel.GenericFlywheel;
-import edu.wpi.team190.gompeilib.subsystems.generic.flywheel.GenericFlywheelIO;
+import edu.wpi.team190.gompeilib.subsystems.generic.roller.GenericRoller;
+import edu.wpi.team190.gompeilib.subsystems.generic.roller.GenericRollerIO;
 
 public class V1_GammaIntake extends SubsystemBase {
-  public GenericFlywheel topRoller;
-  public GenericFlywheel bottomRoller;
+  public GenericRoller topRoller;
+  public GenericRoller bottomRoller;
 
-  public V1_GammaIntake(GenericFlywheelIO topIO, GenericFlywheelIO bottomIO) {
-    topRoller = new GenericFlywheel(topIO, this, "IntakeTopFlywheel");
-    bottomRoller = new GenericFlywheel(bottomIO, this, "IntakeBottomFlywheel");
+  public V1_GammaIntake(GenericRollerIO topIO, GenericRollerIO bottomIO) {
+    topRoller = new GenericRoller(topIO, this, "IntakeTopFlywheel");
+    bottomRoller = new GenericRoller(bottomIO, this, "IntakeBottomFlywheel");
   }
 
   @Override
@@ -29,21 +29,11 @@ public class V1_GammaIntake extends SubsystemBase {
         });
   }
 
-  public Command setVelocityGoal(double velocityGoal) {
-    return Commands.runOnce(
-        () -> {
-          topRoller.setGoal(velocityGoal);
-          bottomRoller.setGoal(velocityGoal);
-        });
-  }
-
   public Command stop() {
     return Commands.runOnce(
         () -> {
           topRoller.setVoltage(0);
           bottomRoller.setVoltage(0);
-          topRoller.setGoal(0);
-          bottomRoller.setGoal(0);
         });
   }
 }
--- src/main/java/frc/robot/subsystems/v1_Gamma/intake/V1_GammaIntakeConstants.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/intake/V1_GammaIntakeConstants.java
@@ -1,78 +1,40 @@
 package frc.robot.subsystems.v1_Gamma.intake;
 
-import com.ctre.phoenix6.signals.InvertedValue;
 import edu.wpi.first.math.system.plant.DCMotor;
-import edu.wpi.team190.gompeilib.core.utility.LoggedTunableNumber;
-import edu.wpi.team190.gompeilib.subsystems.generic.flywheel.GenericFlywheelConstants;
-import edu.wpi.team190.gompeilib.subsystems.generic.flywheel.GenericFlywheelConstants.Constraints;
-import edu.wpi.team190.gompeilib.subsystems.generic.flywheel.GenericFlywheelConstants.Gains;
+import edu.wpi.first.units.Units;
+import edu.wpi.first.units.measure.MomentOfInertia;
+import edu.wpi.team190.gompeilib.subsystems.generic.roller.GenericRollerConstants;
 
 public class V1_GammaIntakeConstants {
-  public static final int[] CAN_IDS_TOP = {16};
+  public static final int CAN_IDS_TOP = 16;
   public static final boolean ON_CANIVORE_TOP = true;
-  public static final boolean ENABLE_FOC_TOP = false;
   public static final double CURRENT_LIMIT_TOP = 30.0;
-  public static final double MOMENT_OF_INERTIA_TOP = 0.0;
+  public static final MomentOfInertia MOMENT_OF_INERTIA_TOP = Units.KilogramSquareMeters.of(67);
   public static final double GEAR_RATIO_TOP = 1.0;
-  public static final DCMotor MOTOR_CONFIG_TOP = new DCMotor(0, 0, 0, 0, 0, 0);
-  public static final Gains GAINS_TOP =
-      new Gains(
-          new LoggedTunableNumber("Ks", 0),
-          new LoggedTunableNumber("Kv", 0),
-          new LoggedTunableNumber("Ka", 0),
-          new LoggedTunableNumber("Kp", 0),
-          new LoggedTunableNumber("Kd", 0));
-  public static final Constraints CONSTRAINTS_TOP =
-      new Constraints(
-          new LoggedTunableNumber("MaxAccelerationRotationsPerSecondSquared", 0),
-          new LoggedTunableNumber("CruisingVelocityRotationsPerSecond", 0),
-          new LoggedTunableNumber("GoalToleranceRotationsPerSecond", 0));
-  public static final InvertedValue MOTOR_INVERTED_TOP = InvertedValue.Clockwise_Positive;
+  public static final DCMotor GEARBOX_TOP = new DCMotor(0, 0, 0, 0, 0, 0);
 
-  public static final GenericFlywheelConstants INTAKE_FLYWHEEL_CONSTANTS_TOP =
-      new GenericFlywheelConstants(
+  public static final GenericRollerConstants INTAKE_ROLLER_CONSTANTS_TOP =
+      new GenericRollerConstants(
           CAN_IDS_TOP,
-          ON_CANIVORE_TOP,
-          ENABLE_FOC_TOP,
           CURRENT_LIMIT_TOP,
-          MOMENT_OF_INERTIA_TOP,
-          GAINS_TOP,
-          MOTOR_CONFIG_TOP,
-          CONSTRAINTS_TOP,
+          GEARBOX_TOP,
           GEAR_RATIO_TOP,
-          MOTOR_INVERTED_TOP);
+          MOMENT_OF_INERTIA_TOP,
+          ON_CANIVORE_TOP);
 
-  public static final int[] CAN_IDS_BOTTOM = {16};
+  public static final int CAN_IDS_BOTTOM = 17;
   public static final boolean ON_CANIVORE_BOTTOM = true;
-  public static final boolean ENABLE_FOC_BOTTOM = false;
   public static final double CURRENT_LIMIT_BOTTOM = 30.0;
-  public static final double MOMENT_OF_INERTIA_BOTTOM = 0.0;
+  public static final MomentOfInertia MOMENT_OF_INERTIA_BOTTOM = Units.KilogramSquareMeters.of(67);
   public static final double GEAR_RATIO_BOTTOM = 1.0;
-  public static final DCMotor MOTOR_CONFIG_BOTTOM = new DCMotor(0, 0, 0, 0, 0, 0);
-  public static final Gains GAINS_BOTTOM =
-      new Gains(
-          new LoggedTunableNumber("Ks", 0),
-          new LoggedTunableNumber("Kv", 0),
-          new LoggedTunableNumber("Ka", 0),
-          new LoggedTunableNumber("Kp", 0),
-          new LoggedTunableNumber("Kd", 0));
-  public static final Constraints CONSTRAINTS_BOTTOM =
-      new Constraints(
-          new LoggedTunableNumber("MaxAccelerationRotationsPerSecondSquared", 0),
-          new LoggedTunableNumber("CruisingVelocityRotationsPerSecond", 0),
-          new LoggedTunableNumber("GoalToleranceRotationsPerSecond", 0));
-  public static final InvertedValue MOTOR_INVERTED_BOTTOM = InvertedValue.Clockwise_Positive;
+  public static final DCMotor GEARBOX_BOTTOM = new DCMotor(0, 0, 0, 0, 0, 0);
 
-  public static final GenericFlywheelConstants INTAKE_FLYWHEEL_CONSTANTS_BOTTOM =
-      new GenericFlywheelConstants(
+  public static final GenericRollerConstants INTAKE_ROLLER_CONSTANTS_BOTTOM =
+      new GenericRollerConstants(
           CAN_IDS_BOTTOM,
-          ON_CANIVORE_BOTTOM,
-          ENABLE_FOC_BOTTOM,
           CURRENT_LIMIT_BOTTOM,
-          MOMENT_OF_INERTIA_BOTTOM,
-          GAINS_BOTTOM,
-          MOTOR_CONFIG_BOTTOM,
-          CONSTRAINTS_BOTTOM,
+          GEARBOX_BOTTOM,
           GEAR_RATIO_BOTTOM,
-          MOTOR_INVERTED_BOTTOM);
+          MOMENT_OF_INERTIA_BOTTOM,
+          ON_CANIVORE_BOTTOM);
 }
