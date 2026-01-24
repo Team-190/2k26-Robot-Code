885bbfaedffee2c46174791a5412ac23175b8901
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2026-01-24 14:16:31 -0500

    Comment

--- src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankConstants.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankConstants.java
@@ -5,7 +5,7 @@ import com.ctre.phoenix6.signals.InvertedValue;
 public class V1_GammaSwankConstants {
 
   public static final double DRIVE_INERTIA = 0.0;
-  public static final double GEAR_RATIO = 52/12 * 24/18 * 15/14; // TODO: make correct
+  public static final double GEAR_RATIO = 52/12 * 24/18 * 15/14;
   public static final int MOTOR_CAN_ID = -1; // TODO: make correct
   public static final double SUPPLY_CURRENT_LIMIT = 40; 
   public static final double STATOR_CURRENT_LIMIT = 40; 
