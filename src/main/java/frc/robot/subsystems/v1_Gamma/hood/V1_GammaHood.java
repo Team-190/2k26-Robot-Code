6f23c8a430fccb906bb1b5e26f91b74116678fa9
jasminepalit <jasmine.palit@gmail.com>
2026-01-24 18:32:35 -0500

    Fized set voltage

--- src/main/java/frc/robot/subsystems/v1_Gamma/hood/V1_GammaHood.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/hood/V1_GammaHood.java
@@ -89,7 +89,7 @@ public class V1_GammaHood {
   public Command setVoltage(double volts) {
     return Commands.runOnce(
         () -> {
-          io.setVoltage(volts);
+          this.voltageGoal = volts;
         });
   }
 
