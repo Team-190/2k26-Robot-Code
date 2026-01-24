9e87b7e0914e0b09fa990203a10780f9680be743
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2026-01-24 13:43:01 -0500

    added climbSequence and wait UntilPosition

--- src/main/java/frc/robot/subsystems/v1_Gamma/climber/V1_GammaClimber.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/climber/V1_GammaClimber.java
@@ -4,14 +4,16 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import org.littletonrobotics.junction.AutoLogOutput;
 import edu.wpi.team190.gompeilib.subsystems.arm.Arm;
 import edu.wpi.team190.gompeilib.subsystems.arm.ArmIO;
+import org.littletonrobotics.junction.AutoLogOutput;
 
-public class V1_GammaClimber extends SubsystemBase{
+public class V1_GammaClimber extends SubsystemBase {
   private Arm arm;
-  
+
+  @AutoLogOutput(key = "Climber/isClimbed")
+  private boolean isClimbed;
+
   public V1_GammaClimber(ArmIO io) {
     arm = new Arm(io, this, 1);
   }
@@ -28,17 +30,26 @@ public class V1_GammaClimber extends SubsystemBase{
   public Command setPosition(Rotation2d position) {
     return Commands.runOnce(() -> arm.setPosition(position));
   }
-  
+
   public Command setPositionGoal(Rotation2d positionGoal) {
     return Commands.runOnce(() -> arm.setPositionGoal(positionGoal));
   }
-  
+
+  public Command waitUntilPosition() {
+    return Commands.waitUntil(
+        () ->
+            Math.abs(arm.inputs.position.getRadians() - arm.inputs.positionGoal.getRadians())
+                < V1_GammaClimberConstants.positionToleranceRadians);
+  }
+
   public Command stop() {
     return Commands.runOnce(() -> arm.setVoltage(0));
   }
 
-  @AutoLogOutput(key = "Climber/isClimbed")
-  private boolean isClimbed;
-  
+  public Command climbSequence() {
+    return Commands.sequence(
+        setPosition(new Rotation2d(V1_GammaClimberConstants.levelOnePositionGoal)),
+        waitUntilPosition(),
+        setPosition(new Rotation2d(V1_GammaClimberConstants.levelTwoPositionGoal)));
+  }
 }
-
