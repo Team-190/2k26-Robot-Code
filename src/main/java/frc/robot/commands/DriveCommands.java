055f21e25d31feaaef663f17227203b6084f48d9
iamabhiveni <asvenigalla@wpi.edu>
2026-01-24 18:15:23 -0500

    Co-authored-by: Anshu <aadiga@wpi.edu> Co-authored-by: LordVanra <LordVanra@users.noreply.github.com> Co-authored-by: Sriaditya Vaddadi <vaddadisri26@users.noreply.github.com>

--- src/main/java/frc/robot/commands/DriveCommands.java
+++ src/main/java/frc/robot/commands/DriveCommands.java
@@ -1,12 +1,9 @@
 package frc.robot.commands;
-
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
@@ -23,49 +20,12 @@ import org.littletonrobotics.junction.Logger;
 
 public final class DriveCommands {
 
-  @Getter private static final ProfiledPIDController alignXController;
-  @Getter private static final ProfiledPIDController alignYController;
-  private static final ProfiledPIDController alignHeadingController;
-
   @Getter private static final PIDController autoXController;
   @Getter private static final PIDController autoYController;
   @Getter private static final PIDController autoHeadingController;
 
 
   static {
-    alignXController =
-        new ProfiledPIDController(
-            DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.xPIDConstants().kP().get(),
-            0.0,
-            DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.xPIDConstants().kD().get(),
-            new TrapezoidProfile.Constraints(
-                DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS
-                    .xPIDConstants()
-                    .maxVelocity()
-                    .get(),
-                Double.POSITIVE_INFINITY));
-    alignYController =
-        new ProfiledPIDController(
-            DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.yPIDConstants().kP().get(),
-            0.0,
-            DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.yPIDConstants().kD().get(),
-            new TrapezoidProfile.Constraints(
-                DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS
-                    .yPIDConstants()
-                    .maxVelocity()
-                    .get(),
-                Double.POSITIVE_INFINITY));
-    alignHeadingController =
-        new ProfiledPIDController(
-            DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.omegaPIDConstants().kP().get(),
-            0.0,
-            DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.omegaPIDConstants().kD().get(),
-            new TrapezoidProfile.Constraints(
-                DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS
-                    .omegaPIDConstants()
-                    .maxVelocity()
-                    .get(),
-                Double.POSITIVE_INFINITY));
 
     autoHeadingController =
         new PIDController(
@@ -79,22 +39,6 @@ public final class DriveCommands {
             DriveConstants.AUTO_GAINS.translation_Kp().get(),
             0.0,
             DriveConstants.AUTO_GAINS.translation_Kd().get());
-
-    alignXController.setTolerance(
-        DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.xPIDConstants().tolerance().get());
-    alignYController.setTolerance(
-        DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.yPIDConstants().tolerance().get());
-
-    alignXController.setTolerance(
-        DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.xPIDConstants().tolerance().get());
-    alignYController.setTolerance(
-        DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.yPIDConstants().tolerance().get());
-
-    alignHeadingController.enableContinuousInput(-Math.PI, Math.PI);
-    alignHeadingController.setTolerance(
-        DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.omegaPIDConstants().tolerance().get());
-    alignHeadingController.setTolerance(
-        DriveConstants.ALIGN_ROBOT_TO_APRIL_TAG_CONSTANTS.omegaPIDConstants().tolerance().get());
 
     autoHeadingController.enableContinuousInput(-Math.PI, Math.PI);
     autoHeadingController.setTolerance(Units.degreesToRadians(1.0));
@@ -161,6 +105,10 @@ public final class DriveCommands {
     return new KSCharacterization(
         drive, drive::runCharacterization, drive::getFFCharacterizationVelocity);
   }
+  
+//   public static Command autoAllignCommand(SwerveDrive drive) {
+//     return new AutoAlignCommand();
+//   }
 
   public static Command wheelRadiusCharacterization(SwerveDrive drive) {
     double WHEEL_RADIUS_MAX_VELOCITY = 0.25; // Rad/Sec
@@ -233,6 +181,8 @@ public final class DriveCommands {
                               + " inches");
                     })));
   }
+
+  
 
   private static class WheelRadiusCharacterizationState {
     double[] positions = new double[4];
