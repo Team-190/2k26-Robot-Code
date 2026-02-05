993352fee583416edf86caa51ac618ba81b26b52
ArnavPrabhudesai <aoprabhudesai@wpi.edu>
2026-02-05 18:30:29 -0500

    Swank moment

--- src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankConstants.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankConstants.java
@@ -4,7 +4,7 @@ import com.ctre.phoenix6.signals.InvertedValue;
 
 public class V1_GammaSwankConstants {
 
-  public static final double DRIVE_INERTIA = 0.0;
+  public static final double DRIVE_INERTIA = 0.004;
   public static final double GEAR_RATIO = 52.0 / 12.0 * 24.0 / 18.0 * 15.0 / 14.0;
   public static final int MOTOR_CAN_ID = 9;
   public static final double SUPPLY_CURRENT_LIMIT = 40;
