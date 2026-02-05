ebc9803808a263dca5b27ac371d577946ecb61e6
ElliotScher <ecscher84@gmail.com>
2026-02-04 22:12:15 -0500

    lint

--- src/main/java/frc/robot/commands/v1_Gamma/autonomous/V1_GammaIntakeTest.java
+++ src/main/java/frc/robot/commands/v1_Gamma/autonomous/V1_GammaIntakeTest.java
@@ -7,6 +7,7 @@ import frc.robot.subsystems.v1_Gamma.intake.V1_GammaIntake;
 public class V1_GammaIntakeTest {
   public static Command testIntake(V1_GammaIntake intake) {
     return Commands.sequence(
-        intake.stow(), Commands.waitSeconds(5), intake.deploy(), Commands.waitSeconds(5)).repeatedly();
+            intake.stow(), Commands.waitSeconds(5), intake.deploy(), Commands.waitSeconds(5))
+        .repeatedly();
   }
 }
