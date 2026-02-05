bb8cc96db04b4eb15941fb7b3b40366597e385a3
ElliotScher <ecscher84@gmail.com>
2026-02-05 10:45:14 -0500

    Add simulation classes for Hood, Turret, and Spindexer subsystems; update constants for inertia and motor configuration

---
+++ src/main/java/frc/robot/subsystems/shared/turret/TurretIOTalonFXSim.java
@@ -0,0 +1,50 @@
+package frc.robot.subsystems.shared.turret;
+
+import static edu.wpi.first.units.Units.Radians;
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import edu.wpi.team190.gompeilib.core.GompeiLib;
+import edu.wpi.team190.gompeilib.core.logging.Trace;
+
+public class TurretIOTalonFXSim extends TurretIOTalonFX {
+    private final DCMotorSim turretSim;
+
+    private TalonFXSimState turretController;
+
+    public TurretIOTalonFXSim() {
+        super();
+
+        turretSim =
+                new DCMotorSim(
+            LinearSystemId.createDCMotorSystem(
+                TurretConstants.MOTOR_CONFIG,
+                TurretConstants.MOMENT_OF_INERTIA,
+                TurretConstants.GEAR_RATIO),
+            TurretConstants.MOTOR_CONFIG);
+    }
+
+    @Override
+    @Trace
+    public void updateInputs(TurretIOInputs inputs) {
+        turretController.setSupplyVoltage(RobotController.getBatteryVoltage());
+        double turretVoltage = turretController.getMotorVoltage();
+
+        turretSim.setInputVoltage(turretVoltage);
+
+        turretSim.update(GompeiLib.getLoopPeriod());
+
+        Angle rotorPosition = Angle.ofBaseUnits(turretSim.getAngularPositionRad() * turretSim.getGearing(), Radians);
+        AngularVelocity rotorVelocity = AngularVelocity.ofBaseUnits(turretSim.getAngularVelocityRadPerSec() * turretSim.getGearing(), RadiansPerSecond);
+        turretController.setRawRotorPosition(rotorPosition);
+        turretController.setRotorVelocity(rotorVelocity);
+
+        super.updateInputs(inputs);
+    }
+}
--- src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankConstants.java
+++ src/main/java/frc/robot/subsystems/v1_Gamma/swank/V1_GammaSwankConstants.java
@@ -2,9 +2,11 @@ package frc.robot.subsystems.v1_Gamma.swank;
 
 import com.ctre.phoenix6.signals.InvertedValue;
 
+import edu.wpi.first.math.system.plant.DCMotor;
+
 public class V1_GammaSwankConstants {
 
-  public static final double DRIVE_INERTIA = 0.0;
+  public static final double MOMENT_OF_INERTIA = 0.004;
   public static final double GEAR_RATIO = 52.0 / 12.0 * 24.0 / 18.0 * 15.0 / 14.0;
   public static final int MOTOR_CAN_ID = -1;
   public static final double SUPPLY_CURRENT_LIMIT = 40;
@@ -12,4 +14,6 @@ public class V1_GammaSwankConstants {
   public static final boolean IS_CAN_FD = false;
 
   public static final InvertedValue INVERSION = InvertedValue.CounterClockwise_Positive;
+
+  public static final DCMotor MOTOR_CONFIG = DCMotor.getKrakenX44Foc(1);
 }
