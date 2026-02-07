553c879549312a44f96e78b5fc416af83b124985
vaddadisri26 <vaddadi.sriaditya@gmail.com>
2026-02-07 15:37:51 -0500

    Renamed/Capitalized "V1_DOOMSPIRAL" and "V1_DOOMSPIRAL_SIM" in RobotType.

--- src/main/java/frc/robot/RobotConfig.java
+++ src/main/java/frc/robot/RobotConfig.java
@@ -1,12 +1,12 @@
 package frc.robot;
 
 public final class RobotConfig {
-  public static final RobotType ROBOT = RobotType.V1_DoomSpiral_SIM;
+  public static final RobotType ROBOT = RobotType.V1_DOOMSPIRAL_SIM;
 
   public enum RobotType {
     V0_FUNKY,
     V0_FUNKY_SIM,
-    V1_DoomSpiral,
-    V1_DoomSpiral_SIM,
+    V1_DOOMSPIRAL,
+    V1_DOOMSPIRAL_SIM,
   }
 }
