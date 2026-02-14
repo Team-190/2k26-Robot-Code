2ebf474ff6a14253e527135eb3aa84b1b6f4ae0e
Anshu Adiga <aadiga@wpi.edu>
2026-02-13 19:13:21 -0500

    pass in roll supplier to climber

--- src/main/java/frc/robot/subsystems/v1_DoomSpiral/climber/V1_DoomSpiralClimber.java
+++ src/main/java/frc/robot/subsystems/v1_DoomSpiral/climber/V1_DoomSpiralClimber.java
@@ -1,25 +1,34 @@
 package frc.robot.subsystems.v1_DoomSpiral.climber;
 
+import static edu.wpi.first.units.Units.Radians;
+
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.team190.gompeilib.subsystems.arm.Arm;
 import edu.wpi.team190.gompeilib.subsystems.arm.ArmIO;
 import frc.robot.subsystems.v1_DoomSpiral.climber.V1_DoomSpiralClimberConstants.ClimberGoal;
+import java.util.function.Supplier;
 import org.littletonrobotics.junction.AutoLogOutput;
 
 public class V1_DoomSpiralClimber extends SubsystemBase {
   private final Arm arm;
+  private final Supplier<Angle> rollSupplier;
+
   private ClimberGoal state;
 
   @AutoLogOutput(key = "Climber/isClimbed")
   private boolean isClimbed; // TODO: Update this
 
-  public V1_DoomSpiralClimber(ArmIO io) {
-    state = ClimberGoal.DEFAULT;
+  public V1_DoomSpiralClimber(ArmIO io, Supplier<Angle> rollSupplier) {
     arm = new Arm(io, this, 1);
-    arm.setPositionGoal(state.getPosition());
+    this.rollSupplier = rollSupplier;
+
+    state = ClimberGoal.DEFAULT;
+
+    setDefaultCommand(setPositionGoal(state.getPosition()));
   }
 
   @Override
@@ -32,7 +41,7 @@ public class V1_DoomSpiralClimber extends SubsystemBase {
   }
 
   public Command setVoltage(double voltage) {
-    return Commands.runOnce(() -> arm.setVoltage(voltage));
+    return runOnce(() -> arm.setVoltage(voltage));
   }
 
   public Command setPosition(Rotation2d position) {
@@ -40,7 +49,7 @@ public class V1_DoomSpiralClimber extends SubsystemBase {
   }
 
   public Command setPositionGoal(Rotation2d positionGoal) {
-    return Commands.runOnce(() -> arm.setPositionGoal(positionGoal));
+    return runOnce(() -> arm.setPositionGoal(positionGoal));
   }
 
   public Command waitUntilPosition() {
@@ -48,8 +57,7 @@ public class V1_DoomSpiralClimber extends SubsystemBase {
   }
 
   public boolean atGoal() {
-    return Math.abs(arm.inputs.position.getRadians() - arm.inputs.positionGoal.getRadians())
-        < V1_DoomSpiralClimberConstants.CONSTRAINTS.goalToleranceRadians().get();
+    return arm.atGoal();
   }
 
   public Command stop() {
@@ -59,11 +67,10 @@ public class V1_DoomSpiralClimber extends SubsystemBase {
   public Command climbSequenceL3() {
     return Commands.sequence(
         Commands.runOnce(() -> state = ClimberGoal.L1_POSITION_GOAL),
-        waitUntilPosition(),
+        setVoltage(12).until(this::atGoal),
         Commands.runOnce(() -> state = ClimberGoal.L2_FLIP_GOAL),
-        waitUntilPosition(),
-        Commands.runOnce(() -> state = ClimberGoal.L2_POSITION_GOAL),
-        waitUntilPosition());
+        setVoltage(-12).until(this::atGoal),
+        setVoltage(12).until(() -> rollSupplier.get().isNear(Radians.of(1), Radians.of(0.1))));
   }
 
   public Command climbAutoSequence() {
