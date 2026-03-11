8a74b4c537f22a4fb555882d818be10d8f624086
ElliotScher <ecscher84@gmail.com>
2026-03-11 14:26:18 -0400

    change gompeilib version to be set dynamically in source code

--- src/main/java/frc/robot/RobotConfig.java
+++ src/main/java/frc/robot/RobotConfig.java
@@ -1,12 +1,22 @@
 package frc.robot;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 public final class RobotConfig {
   public static final RobotType ROBOT = RobotType.V1_DOOMSPIRAL;
 
+  @RequiredArgsConstructor
   public enum RobotType {
-    V0_FUNKY,
-    V0_FUNKY_SIM,
-    V1_DOOMSPIRAL,
-    V1_DOOMSPIRAL_SIM,
+    V0_FUNKY("2026.+"),
+    V0_FUNKY_SIM("2026.+"),
+    V1_DOOMSPIRAL("2026.5.0"),
+    V1_DOOMSPIRAL_SIM("2026.5.0");
+
+    @Getter private String gompeiLibVersion;
+
+    private RobotType(String gompeiLibVersion) {
+      this.gompeiLibVersion = gompeiLibVersion;
+    }
   }
 }
