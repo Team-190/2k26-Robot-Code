54bc0d3cbd62d92b2ff14ad1ecdc59647d624f04
Anshu Adiga <aadiga@wpi.edu>
2026-03-14 11:04:37 -0400

    Update at 'Sat Mar 14 11:04:37 EDT 2026'

--- src/main/java/frc/robot/commands/v1_DoomSpiral/V1_DoomSpiralCompositeCommands.java
+++ src/main/java/frc/robot/commands/v1_DoomSpiral/V1_DoomSpiralCompositeCommands.java
@@ -14,6 +14,7 @@ import frc.robot.subsystems.v1_DoomSpiral.V1_DoomSpiralRobotState.FixedShotParam
 import frc.robot.subsystems.v1_DoomSpiral.shooter.V1_DoomSpiralShooter;
 import frc.robot.subsystems.v1_DoomSpiral.spindexer.V1_DoomSpiralSpindexer;
 import frc.robot.subsystems.v1_DoomSpiral.spindexer.V1_DoomSpiralSpindexerConstants;
+import frc.robot.util.AllianceFlipUtil;
 
 public class V1_DoomSpiralCompositeCommands {
 
@@ -56,7 +57,7 @@ public class V1_DoomSpiralCompositeCommands {
                     drive,
                     V1_DoomSpiralConstants.DRIVE_CONSTANTS,
                     V1_DoomSpiralRobotState::getHeading,
-                    shotParameters.robotAngle()),
+                    () -> AllianceFlipUtil.apply(shotParameters.robotAngle())),
                 shooter.setFlywheelVelocity(shotParameters.flywheelSpeed().in(RadiansPerSecond)),
                 shooter.setOverrideHoodGoal(shotParameters.hoodAngle()))
             .until(shooter::atGoal),
