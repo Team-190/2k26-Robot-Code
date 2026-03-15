a4ce9f75f2a58e4847207757de496e56d28e93ac
ElliotScher <ecscher84@gmail.com>
2026-03-15 15:10:20 -0400

    lint

--- src/main/java/frc/robot/subsystems/v2_Delta/hopper/V2_DeltaHopper.java
+++ src/main/java/frc/robot/subsystems/v2_Delta/hopper/V2_DeltaHopper.java
@@ -1,14 +1,13 @@
 package frc.robot.subsystems.v2_Delta.hopper;
 
+import static edu.wpi.first.units.Units.Volts;
+
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.team190.gompeilib.subsystems.generic.roller.GenericRoller;
 import edu.wpi.team190.gompeilib.subsystems.generic.roller.GenericRollerIO;
-
-import static edu.wpi.first.units.Units.Volts;
-
 import org.littletonrobotics.junction.Logger;
 
 public class V2_DeltaHopper extends SubsystemBase {
