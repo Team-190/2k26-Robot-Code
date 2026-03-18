092cb6ab14ae5a7b292c04eb9975fd034584951c
Anshu Adiga <aadiga@wpi.edu>
2026-03-17 20:26:49 -0400

    fix things

--- src/main/java/frc/robot/subsystems/shared/fourbarlinkage/FourBarLinkageIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/shared/fourbarlinkage/FourBarLinkageIOTalonFX.java
@@ -69,12 +69,8 @@ public class FourBarLinkageIOTalonFX implements FourBarLinkageIO {
         .SoftwareLimitSwitch
         .withForwardSoftLimitThreshold(constants.maxAngle.getRotations())
         .withForwardSoftLimitEnable(true)
-<<<<<<< intake-soft-limit-fix
-        .withReverseSoftLimitThreshold(0)
-=======
         .withReverseSoftLimitThreshold(constants.minAngle.getRotations())
->>>>>>> development
-        .withReverseSoftLimitEnable(false);
+        .withReverseSoftLimitEnable(true);
 
     PhoenixUtil.tryUntilOk(5, () -> talonFX.getConfigurator().apply(talonFXConfig, 0.25));
 
@@ -90,7 +86,7 @@ public class FourBarLinkageIOTalonFX implements FourBarLinkageIO {
 
     // talonFX.setPosition(canCoder.getAbsolutePosition().getValueAsDouble());
 
-    talonFX.setPosition(constants.minAngle.getRotations());
+    talonFX.setPosition(constants.startAngle.getRotations());
     positionRotations = talonFX.getPosition();
     velocity = talonFX.getVelocity();
     torqueCurrent = talonFX.getTorqueCurrent();
