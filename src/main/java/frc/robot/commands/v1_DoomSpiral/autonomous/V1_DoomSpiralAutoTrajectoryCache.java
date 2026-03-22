66ee80e4e57eddd7acccadb9a519c243d4e4e9a8
Rishi <rgandhi@wpi.edu>
2026-03-22 16:07:24 -0400

    added paths that cross midline for simple trench autos

--- src/main/java/frc/robot/commands/v1_DoomSpiral/autonomous/V1_DoomSpiralAutoTrajectoryCache.java
+++ src/main/java/frc/robot/commands/v1_DoomSpiral/autonomous/V1_DoomSpiralAutoTrajectoryCache.java
@@ -27,4 +27,8 @@ public class V1_DoomSpiralAutoTrajectoryCache {
       Choreo.loadTrajectory("LEFT_TRENCH_ANTI_BUCKS_CROSSES").get();
   public static final Trajectory<?> RIGHT_TRENCH_ANTI_BUCKS_CROSSES =
       Choreo.loadTrajectory("RIGHT_TRENCH_ANTI_BUCKS_CROSSES").get();
+  public static final Trajectory<?> LEFT_TRENCH_SIMPLE_CROSSES =
+      Choreo.loadTrajectory("LEFT_TRENCH_SIMPLE_CROSSES").get();
+    public static final Trajectory<?> RIGHT_TRENCH_SIMPLE_CROSSES =
+      Choreo.loadTrajectory("RIGHT_TRENCH_SIMPLE_CROSSES").get();
 }
