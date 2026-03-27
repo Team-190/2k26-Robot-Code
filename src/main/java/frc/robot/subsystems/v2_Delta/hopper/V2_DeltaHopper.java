cb7aee6358d7914ecd71db7dbce4b2eafdc7d172
iamabhiveni <asvenigalla@wpi.edu>
2026-03-26 20:49:01 -0400

    Prelim changes for V2_DeltaHopper climber logic

--- src/main/java/frc/robot/subsystems/v2_Delta/hopper/V2_DeltaHopper.java
+++ src/main/java/frc/robot/subsystems/v2_Delta/hopper/V2_DeltaHopper.java
@@ -2,16 +2,23 @@ package frc.robot.subsystems.v2_Delta.hopper;
 
 import static edu.wpi.first.units.Units.Volts;
 
+import java.util.function.BooleanSupplier;
+
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.team190.gompeilib.subsystems.generic.roller.GenericRoller;
 import edu.wpi.team190.gompeilib.subsystems.generic.roller.GenericRollerIO;
+import edu.wpi.first.wpilibj.DigitalInput;
 
 public class V2_DeltaHopper extends SubsystemBase {
   private final GenericRoller rollerFloor;
   private final GenericRoller ballTunnel;
+  private DigitalInput beamBreak;
+
+  private BooleanSupplier beamBreakTrue = () -> beamBreak.get();
+  private BooleanSupplier beamBreakFalse = () -> !beamBreak.get();
 
   public V2_DeltaHopper(GenericRollerIO rollerFloorIO, GenericRollerIO ballTunnelIO) {
     setName("Hopper");
@@ -69,4 +76,32 @@ public class V2_DeltaHopper extends SubsystemBase {
   public Command outtake() {
     return Commands.parallel(outtakeRollerFloor(), outtakeBallTunnel());
   }
+
+  public Command deployClimber() {
+    return Commands.sequence(
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(12))).until(beamBreakFalse),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(12))).until(beamBreakTrue),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(6))).until(beamBreakFalse),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(6))).until(beamBreakTrue),
+      Commands.runOnce(() -> rollerFloor.setVoltageGoal(Volts.of(0.0))));
+  }
+
+  public Command climbUp() {
+    return Commands.sequence(
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(12))).until(beamBreakTrue),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(12))).until(beamBreakFalse),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(6))).until(beamBreakTrue),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(0.0)))
+    );
+  }
+
+  public Command climbDown() {
+    return Commands.sequence(
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(6))).until(beamBreakFalse),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(6))).until(beamBreakTrue),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(3))).until(beamBreakFalse),
+      Commands.run(() -> rollerFloor.setVoltageGoal(Volts.of(0)))
+    );
+  }
+
 }
