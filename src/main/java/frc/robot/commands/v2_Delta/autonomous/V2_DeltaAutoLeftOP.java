8362fefbfd813518b0f117d5acbc85364fb27534
ananthgomattam <akgomattam@wpi.edu>
2026-04-14 23:33:33 -0400

    IT FUCKING WORKS BABY BANGGG

--- src/main/java/frc/robot/commands/v2_Delta/autonomous/V2_DeltaAutoLeftOP.java
+++ src/main/java/frc/robot/commands/v2_Delta/autonomous/V2_DeltaAutoLeftOP.java
@@ -1,19 +1,22 @@
 package frc.robot.commands.v2_Delta.autonomous;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 import com.pathplanner.lib.path.PathPlannerPath;
-import com.pathplanner.lib.util.FileVersionException;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.team190.gompeilib.subsystems.drivebases.swervedrive.SwerveDrive;
 import frc.robot.commands.v2_Delta.V2_DeltaCompositeCommands;
 import frc.robot.subsystems.shared.intake.Intake;
 import frc.robot.subsystems.shared.intake.IntakeConstants;
+import frc.robot.subsystems.v2_Delta.V2_DeltaConstants;
 import frc.robot.subsystems.v2_Delta.V2_DeltaRobotState;
 import frc.robot.subsystems.v2_Delta.clopper.V2_DeltaClopper;
 import frc.robot.subsystems.v2_Delta.shooter.V2_DeltaShooter;
-import java.io.IOException;
-import org.json.simple.parser.ParseException;
+import frc.robot.util.AllianceFlipUtil;
 
 public class V2_DeltaAutoLeftOP {
   public static Command getAutoRoutine(
@@ -21,11 +24,36 @@ public class V2_DeltaAutoLeftOP {
 
     PathPlannerPath OP_SAFE_1;
     try {
+      AutoBuilder.configure(
+          V2_DeltaRobotState::getGlobalPose,
+          V2_DeltaRobotState::resetPose,
+          drive::getMeasuredChassisSpeeds,
+          drive::runVelocity,
+          new PPHolonomicDriveController(
+              new PIDConstants(
+                  V2_DeltaConstants.DRIVE_CONSTANTS.autoTranslationGains.kP().getAsDouble(),
+                  V2_DeltaConstants.DRIVE_CONSTANTS.autoTranslationGains.kI().getAsDouble(),
+                  V2_DeltaConstants.DRIVE_CONSTANTS.autoTranslationGains.kD().getAsDouble()),
+              new PIDConstants(
+                  V2_DeltaConstants.DRIVE_CONSTANTS.autoRotationGains.kP().getAsDouble(),
+                  V2_DeltaConstants.DRIVE_CONSTANTS.autoRotationGains.kI().getAsDouble(),
+                  V2_DeltaConstants.DRIVE_CONSTANTS.autoRotationGains.kD().getAsDouble())),
+          RobotConfig.fromGUISettings(),
+          () -> {
+            var alliance = DriverStation.getAlliance();
+            if (alliance.isPresent()) {
+              return alliance.get() == DriverStation.Alliance.Red;
+            }
+            return false;
+          },
+          drive);
       OP_SAFE_1 = PathPlannerPath.fromPathFile("LEFT_OP_SAFE_1");
       PathPlannerPath OP_2 = PathPlannerPath.fromPathFile("LEFT_OP_2");
       return Commands.sequence(
           Commands.runOnce(
-              () -> V2_DeltaRobotState.resetPose(OP_SAFE_1.getStartingHolonomicPose().get())),
+              () ->
+                  V2_DeltaRobotState.resetPose(
+                      AllianceFlipUtil.apply(OP_SAFE_1.getStartingHolonomicPose().get()))),
           intake
               .deploy()
               .alongWith(intake.setOverrideRollerVoltage(IntakeConstants.INTAKE_VOLTAGE)),
@@ -36,7 +64,7 @@ public class V2_DeltaAutoLeftOP {
               .alongWith(intake.deploy(), V2_DeltaCompositeCommands.hold(clopper, shooter)),
           Commands.runOnce(drive::stop),
           V2_DeltaCompositeCommands.scoreOrFeedCommand(shooter, clopper));
-    } catch (FileVersionException | IOException | ParseException e) {
+    } catch (Exception e) {
       e.printStackTrace();
       return Commands.none();
     }
